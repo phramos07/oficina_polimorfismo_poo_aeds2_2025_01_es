4b42a31a3ad462f9769c7d523a55085ae7b707fa
Pedro Ramos <pedroramos@Pedros-MacBook-Pro-3.local>
2025-02-13 14:58:14 -0300

    new:  especificacao do trabalho

--- src/ProdutoTest.java
+++ src/ProdutoTest.java
@@ -1,7 +1,4 @@
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +20,7 @@ public class ProdutoTest {
     @Test
     public void stringComDescricaoEValor(){
         String desc = produto.toString();
-        assertTrue(desc.contains("Produto teste") && desc.contains("R$ 110,00"));
+        assertTrue(desc.contains("Produto teste") && desc.contains("R$ 110,00"));
     }
 
     @Test
